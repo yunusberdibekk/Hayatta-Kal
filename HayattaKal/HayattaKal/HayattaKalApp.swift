75260f5709233f60449eec76e8cb55a04e59315f
Yunus Emre Berdibek <com.yunusberdibekk@gmail.com>
2024-04-04 11:40:04 +0300

    Project created.

---
+++ HayattaKal/HayattaKal/HayattaKalApp.swift
@@ -0,0 +1,17 @@
+//
+//  HayattaKalApp.swift
+//  HayattaKal
+//
+//  Created by Yunus Emre Berdibek on 4.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct HayattaKalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
